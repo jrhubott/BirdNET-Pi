810e293707dbaee8c65537fd4d8f4f008f9e9e04
Brian Dembinski <brian.dembinski@macys.com>
2024-07-23 11:48:09 -0400

    Cleanup

--- scripts/createdb.sh
+++ scripts/createdb.sh
@@ -17,7 +17,6 @@ CREATE TABLE IF NOT EXISTS detections (
   File_Name VARCHAR(100) NOT NULL);
 CREATE INDEX "detections_Com_Name" ON "detections" ("Com_Name");
 CREATE INDEX "detections_Date_Time" ON "detections" ("Date" DESC, "Time" DESC);
-
 EOF
 chown $USER:$USER $HOME/BirdNET-Pi/scripts/birds.db
 chmod g+w $HOME/BirdNET-Pi/scripts/birds.db
