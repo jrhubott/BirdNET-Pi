0c017369a02fdaf5b9712c7c9ae764aa31828b46
Jason Rhubottom <jason@rhusoft.com>
2024-08-14 20:45:49 -0700

    Make paths relative

--- scripts/utils/helpers.py
+++ scripts/utils/helpers.py
@@ -42,7 +42,7 @@ def get_settings(settings_path='/etc/birdnet/birdnet.conf', force_reload=False):
 
 
 class Detection:
-    def __init__(self, start_time, stop_time, species, confidence):
+    def __init__(self, start_time, stop_time, species, confidence, file_name):
         self.start = float(start_time)
         self.stop = float(stop_time)
         self.confidence = round(float(confidence), 4)
@@ -51,6 +51,7 @@ class Detection:
         self.scientific_name = species.split('_')[0]
         self.common_name = species.split('_')[1]
         self.common_name_safe = self.common_name.replace("'", "").replace(" ", "_")
+        self.file_name = file_name
         self.file_name_extr = None
 
 
