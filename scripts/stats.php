aa95e78f766240c9d5fd73a7873b883d2927a356
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-01-22 11:24:11 +0100

    Add new custom audio player

--- scripts/stats.php
+++ scripts/stats.php
@@ -149,6 +149,7 @@ if (get_included_files()[0] === __FILE__) {
   <button onclick="hideDialog()">Close</button>
 </dialog>
 <script src="static/dialog-polyfill.js"></script>
+<script src="static/custom-audio-player.js"></script>
 <script>
 var dialog = document.querySelector('dialog');
 dialogPolyfill.registerDialog(dialog);
@@ -201,8 +202,8 @@ while($results=$result3->fetchArray(SQLITE3_ASSOC)){
   Occurrences: $count<br>
   Max Confidence: $maxconf<br>
   Best Recording: $date $time<br><br>
-  <video onplay='setLiveStreamVolume(0)' onended='setLiveStreamVolume(1)' onpause='setLiveStreamVolume(1)' controls poster=\"$filename.png\" title=\"$filename\"><source src=\"$filename\"></video></td>
-  </tr>
+  <div class='custom-audio-player' data-audio-src=\"$filename\" data-image-src=\"$filename.png\"></div>
+  </td></tr>
     </table>
   <p>Loading Images from Flickr</p>", '6096');
   
@@ -251,8 +252,9 @@ array_push($excludelines, $results['Date']."/".$comname."/".$results['File_Name'
       <td class="relative"><a target="_blank" href="index.php?filename=<?php echo $results['File_Name']; ?>"><img title="Open in new tab" class="copyimage" width=25 src="images/copy.png"></a>
         <button type="submit" name="species" value="<?php echo $results['Com_Name'];?>"><?php echo $results['Com_Name'];?></button><br><b>Occurrences:</b> <?php echo $results['COUNT(*)'];?><br>
       <b>Max Confidence:</b> <?php echo $percent = round((float)round($results['MAX(Confidence)'],2) * 100 ) . '%';?><br>
-      <b>Best Recording:</b> <?php echo $results['Date']." ".$results['Time'];?><br><video onplay='setLiveStreamVolume(0)' onended='setLiveStreamVolume(1)' onpause='setLiveStreamVolume(1)' controls poster="<?php echo $filename.".png";?>" preload="none" title="<?php echo $filename;?>"><source src="<?php echo $filename;?>" type="audio/mp3"></video></td>
-      </tr>
+      <b>Best Recording:</b> <?php echo $results['Date']." ".$results['Time'];?><br>
+      <div class='custom-audio-player' data-audio-src="<?php echo $filename;?>" data-image-src="<?php echo $filename.".png";?>"></div>
+      </td></tr>
 <?php
 }
 
